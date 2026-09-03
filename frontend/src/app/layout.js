74f5fb11eb40e1372f075b367639052056161a5d
mehak iqbal <mehakiqbaliqbal974@gmail.com>
2026-09-03 16:19:38 +0500

    fix: load popunder inline to ensure execution

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -120,8 +120,10 @@ export default function RootLayout({ children }) {
         <SpeedInsights />
         <Script
           id="popunder"
-          src="/popunder.js"
           strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(jmwxc){var d=document,s=d.createElement('script'),l=d.scripts[d.scripts.length-1];s.settings=jmwxc||{};s.src="\\/\\/smooth-survey.com\/c.Du9t6_bT2h5\/lfSOWpQQ9QN\/zxM\/3\/OGT\/kw1TMtyu0z3OMpzzco5\/OqT\/Ud3m";s.async=true;s.referrerPolicy='no-referrer-when-downgrade';l.parentNode.insertBefore(s,l);})({})`,
+          }}
         />
         <Script
           id="inpush"
